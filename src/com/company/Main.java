11024329f742d8294851e9ba93fb65fa1124fb22
DashaUrbanovich <nrj_100@mail.ru>
2022-02-01 20:58:31 +0300

    add class and interface

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+Person person=new Person();
+person.eat();
+person.breathe();
+person.holdBreathe();
+person.sneeze();
+Animal animal=new Animal();
+animal.eat();
+animal.breathe();
+animal.holdBreathe();
+animal.sneeze();
+
+
     }
+
+    public void breath(Breathing b) {
+        b.breathe();
+        b.holdBreathe();
+        b.sneeze();
+
+    }
+
+
 }
